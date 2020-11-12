7fee5e39ee0cb1e1f1d5cb74bc985163566f5e60
Vincent-Boateng <vincentboateng@gmail.com>
2020-11-12 11:21:38 -0500

    just ran the code, and it passed.

--- src/Cocomelon.java
+++ src/Cocomelon.java
@@ -1,6 +1,14 @@
 public class Cocomelon {
 
     public static void main(String[] args) {
+// write your code here
+
+        int a = 10;
+
+        int b = 12;
+
+        System.out.println(a + b);
+
 
     }
 }
